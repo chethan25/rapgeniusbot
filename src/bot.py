c9314347b046e464105e0aeb9a16d23dca74a48c
Chethan B <chethan25.bpac@gmail.com>
2020-05-27 15:35:49 +0530

    handled exceptions

--- src/bot.py
+++ src/bot.py
@@ -4,12 +4,15 @@ Module to stream reddit comments and reply with song
 lyrics, metadata when triggered.
 """
 import os
+import re
 import time
 import json
 import shutil
 
 import praw
 import lyricsgenius
+from prawcore.exceptions import RequestException, Forbidden, ServerError
+from praw.exceptions import APIException, ClientException
 
 import database as db
 
@@ -40,14 +43,23 @@ def main():
                 # Parse comments for song name, artist name and option.
                 comment_list = comment.body.split(',')
 
-                artist_name = comment_list[1].strip().lower()
-                song_name = comment_list[2].strip().lower()
-                option = comment_list[3].strip().lower()
+                try:
+                    artist_name = comment_list[1].strip().lower()
+                    song_name = comment_list[2].strip().lower()
+                    option = comment_list[3].strip().lower()
+                    option_list = ['lyrics', 'short info',
+                                   'long info', 'relations']
+                    if option not in option_list:
+                        print("Invalid Option")
+                        continue
+                except IndexError:
+                    print("Invalid comment format")
+                    continue
 
                 new_artist_name = artist_name.replace(
                     " ", "").replace("'", "").replace("-", "").replace('"', "")
                 new_song_name = song_name.replace(" ", "").replace(
-                    "'", "").replace("(", "").replace(")", "")
+                    "'", "").replace("(", "").replace(")", "").replace("&, "")
 
                 # Construct the json filename.
                 filename = f"lyrics_{new_artist_name}_{new_song_name}.json"
@@ -69,13 +81,35 @@ def main():
                     break
 
                 else:
-                    # Save the songs json file.
-                    genius.search_song(song_name,
-                                       artist=artist_name,
-                                       get_full_info=True).save_lyrics()
+                    try:
+                        # Save the songs json file.
+                        genius.search_song(song_name,
+                                           artist=artist_name,
+                                           get_full_info=True).save_lyrics()
 
-                    # Move json file to lyrics directory.
-                    shutil.move(filename, dest_path)
+                        for filename in os.listdir('.'):
+                            if filename[-4:] == 'json':
+                                filename_list = re.split(r'[_.]', filename)
+
+                        artist_name = filename_list[1]
+                        song_name = filename_list[2]
+
+                        # Construct the json filename.
+                        filename = f"lyrics_{artist_name}_{song_name}.json"
+
+                        # Find the absolute path to json file.
+                        dest_path = os.path.abspath(f"lyrics/{filename}")
+
+                    except AttributeError:
+                        print("Invalid Song Request")
+                        continue
+
+                    try:
+                        # Move json file to lyrics directory.
+                        shutil.move(filename, dest_path)
+                    except FileNotFoundError:
+                        print("Invalid Song Request")
+                        continue
 
                     if option == 'lyrics':
                         post_lyrics(dest_path, comment)
@@ -92,8 +126,8 @@ def post_lyrics(d_path, comment):
     """Parse json file for songs lyrics and reply lyrics to the comment."""
     with open(d_path) as f:
         data = json.load(f)
-        comment.reply(
-            f"**LYRICS**\n\n---\n\n{data.get('lyrics', 'Lyrics Unavailable')}")
+        comment.reply(f"**\"{data.get('title').upper()}\"** **LYRICS**\
+                \n\n---\n\n{data.get('lyrics', 'Lyrics Unavailable')}")
         add_entry(comment)
         print('posted')
 
@@ -165,8 +199,8 @@ def post_long_song_info(d_path, comment):
     if recorded_at == None:
         recorded_at = ""
 
-    comment.reply(
-        f"**TRACK INFO**\n\n---\n\n**Writer Artists** - {writer_artists}\
+    comment.reply(f"**\"{data.get('title').upper()}\"** **TRACK INFO**\
+            \n\n---\n\n**Writer Artists** - {writer_artists}\
             \n\n{custom_performance_str}\n\n**Recorded At** - {recorded_at}")
     add_entry(comment)
     print('posted')
@@ -190,9 +224,11 @@ def post_song_relations(d_path, comment):
                 'type'), []).append(song.get('full_title'))
     song_relationships_str = ''
     for key, value in song_relationships_dict.items():
-        song_relationships_str += f"**{key.title().replace('_', ' ')}** - {', '.join(value)}" + '\n\n'
+        song_relationships_str += \
+            f"**{key.title().replace('_', ' ')}** - {', '.join(value)}" + '\n\n'
 
-    comment.reply(f"**TRACK RELATIONSHPS**\n\n---\n\n{song_relationships_str}")
+    comment.reply(f"**\"{data.get('title').upper()}\"** **TRACK RELATIONSHPS**\
+            \n\n---\n\n{song_relationships_str}"))
     add_entry(comment)
     print('posted')
 
@@ -200,7 +236,7 @@ def post_song_relations(d_path, comment):
 def is_added(comment_id):
     """Check if comment id is present in the database."""
     try:
-        comments1 = db.Comments.get(db.Comments.cid == comment_id)
+        comments1=db.Comments.get(db.Comments.cid == comment_id)
         return True
     except:
         return False
@@ -210,15 +246,18 @@ def add_entry(comment_id):
     """Add comment id to the database if it wasn't already in the database."""
     if not is_added(comment_id):
         print(f"Adding {comment_id}")
-        db.Comments(cid=comment_id).save()
+        db.Comments(cid = comment_id).save()
 
 
 def flush_db():
     """Delete all comment ids from database."""
-    coms = db.Comments.select()
+    coms=db.Comments.select()
     for com in coms:
         com.delete_instance()
 
 
 if __name__ == "__main__":
-    main()
+    try:
+        main()
+    except RequestException:
+        print("There was an ambiguous exception that occurred while handling your request")
